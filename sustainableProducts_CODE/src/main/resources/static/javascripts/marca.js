203fcff6d6a4ba59c0df53ff4810a99f19fad8c1
Edgar Neto <edgar100neto@gmail.com>
2021-01-06 16:27:29 +0000

    Chamada de produtos de uma determinada marca

--- sustainableProducts_CODE/src/main/resources/static/javascripts/marca.js
+++ sustainableProducts_CODE/src/main/resources/static/javascripts/marca.js
@@ -1,35 +1,44 @@
+var Marca;
 window.onload = async function () {
-    let prods = document.getElementById("produtos");
+    let prods = document.getElementById("Marca_produtos");
     let cats = document.getElementById("categorias");
     let marcs = document.getElementById("marcas");
+
+    Marca= sessionStorage.getItem("marca");
     try {
         
         //A Chamar os produtos na galeria
         let produtos = await $.ajax({
-            url: "/api/categorias/produtos",
+            url: "/api/categorias/produtos/"+Marca,
             method: "get",
             dataType: "json"
         });
         let html = "";
         let html3 = "";
-        var marca = "";
-        let m = [];
-        for (let prod of produtos){
+
+        for (let prod of produtos)
             html += ` <figure class="gallery-frame" id=${prod.id} ><img class="gallery-img" src="../images/logoSP.jpg"><figcaption>${prod.nome} - preço: €${prod.preco}</figcaption></figure>`
-            //A chamar as marcas
-            
-            if(!m.includes(prod.marca)){
-                m.push(prod.marca)
-                marca = prod.marca
-                html3 += ` <a href="marca.html" onclick='showMarca("${marca}")' > ${marca} </a>`
-            }
+        
+        prods.innerHTML= html
+        
+        
+        let produtosM = await $.ajax({
+            url: "/api/categorias/produtos",
+            method: "get",
+            dataType: "json"
+        });
 
-    
-            
+        let m=[]
+        var marc=""
+        for (let prod of produtosM){
 
+        if(!m.includes(prod.marca)){
+            m.push(prod.marca)
+            marc = prod.marca
+            html3 += ` <a href="marca.html" onclick='showMarca("${marc}")' > ${marc} </a>`
         }
-        marcs.innerHTML = html3;
-        prods.innerHTML = html;
+    }
+     marcs.innerHTML= html3
             
     
     
@@ -53,13 +62,13 @@ window.onload = async function () {
 }
 
 
-
 function showCategoria(idcat) {
     sessionStorage.setItem("categoriaId", idcat);
-    //window.location = "categoria.html";
+   
 }
 
 function showMarca(marc) {
     sessionStorage.setItem("marca", marc);
     
+   
 }
